164ac35a2b84c14cd60ff6048114cbd1a3d70464
levanovroman <levanovroman2016@yandex.ru>
2024-02-06 11:12:08 +0300

    3.7 done

--- src/main/java/hibernate_test/Test3.java
+++ src/main/java/hibernate_test/Test3.java
@@ -5,7 +5,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+import java.util.List;
+
+public class Test3 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -14,10 +16,16 @@ public class Test1 {
 
         try {
             Session session = factory.getCurrentSession();
-            Employee emp = new Employee("Aleksandr", "Ivanov", "IT", 600);
             session.beginTransaction();
-            session.save(emp);
+
+//            List<Employee> emps = session.createQuery("from Employee").getResultList();
+
+            List<Employee> emps = session.createQuery("from Employee where name = 'Aleksandr' AND salary>650")
+                    .getResultList();
+
+            for (Employee e : emps) System.out.println(e);
             session.getTransaction().commit();
+
             System.out.println("DONE!");
         }
         finally{
